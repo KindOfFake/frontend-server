ddd1e451dd24c36481b9d9fec088970a5e798f78
Abhishek Kumar <80272610+hectic-oslo@users.noreply.github.com>
2022-02-02 18:34:38 +0530

    jhh

---
+++ build/precache-manifest.c2645421a9ec0d959d595da62cf1d5a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f43d98672dac5bc1dfa829a21f8ce18d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ecc3d7f69bbdc4a68c1",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "ed199d57d81dc5d7d77f",
+    "url": "/static/css/main.1d4a7b4d.chunk.css"
+  },
+  {
+    "revision": "1ecc3d7f69bbdc4a68c1",
+    "url": "/static/js/2.9c89422b.chunk.js"
+  },
+  {
+    "revision": "30cc64e459902f389fed",
+    "url": "/static/js/3.63e3b4f9.chunk.js"
+  },
+  {
+    "revision": "ed199d57d81dc5d7d77f",
+    "url": "/static/js/main.97fb4e86.chunk.js"
+  },
+  {
+    "revision": "953780f62cdac0bdf31a",
+    "url": "/static/js/runtime~main.8bbd8ede.js"
+  }
+]);
